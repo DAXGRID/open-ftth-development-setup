07510ae263593e3b4094296e0d1b6d4f75dc0dcd
Rune Nielsen <runeanielsen@live.dk>
2020-06-01 23:20:19 +0200

    adds cassandra to setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-helm repo add confluentinc https://confluentinc.github.io/cp-helm-charts/   #(1)
-helm repo update    #(2)
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add confluentinc https://confluentinc.github.io/cp-helm-charts/
+helm repo update
 
 kubectl create ns openftth
 
-helm install openftth --set cp-schema-registry.enabled=false,cp-kafka-rest.enabled=false,cp-control-center.enabled=false confluentinc/cp-helm-charts --namespace openftth
+helm install openftth-kafka --set cp-schema-registry.enabled=false,cp-kafka-rest.enabled=false,cp-control-center.enabled=false confluentinc/cp-helm-charts --namespace openftth
+
+helm install openftth-cassandra bitnami/cassandra --namespace openftth
