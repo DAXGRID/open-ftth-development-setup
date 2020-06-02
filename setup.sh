2837ffef3ea40582112214106b9eadc65cb88855
Rune Nielsen <runeanielsen@live.dk>
2020-06-02 13:51:32 +0200

    changes to strimzi

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo add confluentinc https://confluentinc.github.io/cp-helm-charts/
 helm repo update
 
 kubectl create ns openftth
 
-helm install openftth-kafka --set cp-schema-registry.enabled=false,cp-kafka-rest.enabled=false,cp-control-center.enabled=false confluentinc/cp-helm-charts --namespace openftth
+kubectl apply -f 'https://strimzi.io/install/latest?namespace=openftth' -n openftth
+kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml -n openftth
 
-helm install openftth-cassandra bitnami/cassandra --namespace openftth
+helm install openftth-cassandra bitnami/cassandra -n openftth
 
-kubectl apply -f ./postgis.yaml --namespace openftth
+kubectl apply -f ./postgis.yaml -n openftth
