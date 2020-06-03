124c0c5811ca6cadcbddf53df83b11da0897c146
Rune Nielsen <runeanielsen@live.dk>
2020-06-03 15:18:28 +0200

    updates setup file with correct paths

--- setup.sh
+++ setup.sh
@@ -3,19 +3,23 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
+# Create project namespace
 kubectl create ns openftth
 
-kubectl apply -f 'https://strimzi.io/install/latest?namespace=openftth' -n openftth
-kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml \
-        -n openftth
-
+# Setup cassandra
 helm install openftth-cassandra bitnami/cassandra -n openftth
 
-kubectl apply -f ./postgis.yaml -n openftth
+# Setup Postgis
+kubectl apply -f ./postgis/postgis.yaml -n openftth
 
+# Create Strimzi Kafka   
+kubectl apply -f 'https://strimzi.io/install/latest?namespace=openftth' -n openftth
+kubectl apply -f 'https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml' -n openftth
+
+# Create secret for postgres connector
 kubectl -n openftth create secret generic postgres-credentials \
   --from-file=./secrets/debezium-postgres-credentials.properties
 
-kubectl create -f ./connectors/postgres-connect-cluster.yaml
-
-kubectl create -f ./connectors/postgis-connector.yaml
+# Create postgres connector for postgis
+kubectl create -f ./connectors/postgres-connect-cluster.yaml -n openftth
+kubectl create -f ./connectors/postgis-connector.yaml -n openftth
