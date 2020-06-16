adf9cfaf4d1db7f25f333c135fad0c8100b23f4a
Rune Nielsen <runeanielsen@live.dk>
2020-06-17 00:41:02 +0200

    adds janus graph

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,16 +5,22 @@ kubectl create namespace openftth
 
 # Install Strimzi
 helm repo add strimzi https://strimzi.io/charts/
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com
+
 helm repo update
+
 helm install strimzi strimzi/strimzi-kafka-operator \
    --namespace openftth \
    --version 0.18
 
-# Install OpenFTTH
-
 # This is needed to make sure that the strimzi custom types are being registered
 sleep 1s
 
+# Build dependencies
+helm dependencies build openftth
+
+# Install OpenFTTH
 helm install openftth openftth/ \
   --namespace openftth
 
