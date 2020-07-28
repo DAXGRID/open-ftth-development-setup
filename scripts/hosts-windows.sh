bd39c0103a3d27dcbdc6aae51f264ed2adbef2ec
Jesper Ladegaard <jesper@dax.dk>
2020-07-28 16:58:35 +0200

    update windows script

--- scripts/hosts-windows.sh
+++ scripts/hosts-windows.sh
@@ -3,15 +3,15 @@
 path=c:/windows/system32/drivers/etc/hosts
 
 minikube=$(minikube ip)
-postgis=$minikube:$(kubectl describe service openftth-postgis -n openftth | grep NodePort | grep -o '[0-9]\+')
+postgis=$(kubectl describe service openftth-postgis -n openftth | grep NodePort | grep -o '[0-9]\+')
 
 kafka=$minikube:$(kubectl describe service openftth-kafka-cluster-kafka-external-bootstrap -n openftth | grep NodePort | grep -o '[0-9]\+')
 
-echo "Exporting $postgis with netsh"
-netsh interface portproxy add v4tov4 listenport=5432 listenaddress=127.65.43.21 connectport=$postgis connectaddress=$minikube
+echo "Exporting postgis port $postgis with netsh"
+netsh interface portproxy add v4tov4 listenport=5000 listenaddress=127.65.43.25 connectport=$postgis connectaddress=$minikube
 
-echo "Exporting $kafka to environment variable KAFKA_HOST"
+echo "Exporting kafka $kafka to environment variable KAFKA_HOST"
 setx KAFKA_HOST $kafka
 
 sed -i '/openftth/d' $path
-echo "127.65.43.21 openftth" >> $path
+echo "127.65.43.25 openftth" >> $path
