aeea229e58ceada2b769c5fd66f2c46738969701
Rune Nielsen <runeanielsen@live.dk>
2020-08-24 11:36:45 +0200

    updates clean script

--- scripts/dev/clean.sh
+++ scripts/dev/clean.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-cmd  "psql -h 116.203.155.79 -p 32192 -U postgres -d OPEN_FTTH -a -f ./scripts/dev/truncate_tables.sql"
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+POSTGRES_HOST=$1
+POSTGRES_PORT=$(kubectl describe service openftth-postgis -n openftth | grep NodePort | grep -o '[0-9]\+')
+POSTGRES_DATABASE="OPEN_FTTH"
+POSTGRES_USERNAME="postgres"
+POSTGRES_PASSWORD="postgres"
+
+sql="$(cat truncate_tables.sql)"
+
+# If psql is not installed, then exit
+if ! command -v psql > /dev/null; then
+  echo "PostgreSQL is required..."
+  exit 1
+fi
+
+PGPASSWORD="$POSTGRES_PASSWORD" psql -t -A \
+-h "$POSTGRES_HOST" \
+-p "$POSTGRES_PORT" \
+-d "$POSTGRES_DATABASE" \
+-U "$POSTGRES_USERNAME" \
+-c "$sql"
+
