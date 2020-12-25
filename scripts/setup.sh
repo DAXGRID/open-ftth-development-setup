b0a9e8aceaaa3cff17f34307ca61654e50358c2d
Rune Nielsen <runenielsen@runbox.com>
2020-12-25 17:36:24 +0100

    fixes where repo was pointing to deprecated repo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ kubectl create namespace openftth
 
 # Install Strimzi
 helm repo add strimzi https://strimzi.io/charts/
-helm repo add grafana https://grafana.github.io/loki/charts
+helm repo add grafana https://grafana.github.io/helm-charts
 
 helm repo update
 
@@ -13,7 +13,7 @@ helm repo update
 helm install strimzi strimzi/strimzi-kafka-operator -n openftth --version 0.19
 
 # Install loki
-helm upgrade --install loki --namespace=openftth grafana/loki-stack  --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false,loki.persistence.enabled=true,loki.persistence.storageClassName=standard,loki.persistence.size=10Gi --version jk
+helm upgrade --install loki --namespace=openftth grafana/loki-stack --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false,loki.persistence.enabled=true,loki.persistence.storageClassName=standard,loki.persistence.size=10Gi
 
 # This is needed to make sure that the strimzi custom types are being registered
 sleep 1s
