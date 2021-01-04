d388670467a30f83fa3778ffe4043f6ab6b97538
Rune Nielsen <runenielsen@runbox.com>
2021-01-04 18:50:47 +0100

    now uses /usr/bin/env bash instead

--- scripts/hosts-windows.sh
+++ scripts/hosts-windows.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 path=c:/windows/system32/drivers/etc/hosts
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Create namespace
 kubectl create namespace openftth
