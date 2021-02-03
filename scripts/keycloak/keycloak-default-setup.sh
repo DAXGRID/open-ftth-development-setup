2c92354b3c816afd49e0395c9c4b2bd8a1ac2a86
Rune Nielsen <runenielsen@runbox.com>
2021-02-03 13:27:58 +0100

    adds default setup for keycloak for testing (#30)

---
+++ scripts/keycloak/keycloak-default-setup.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+NEW_REALM="openftth"
+KEYCLOAK_URL=http://$(kubectl describe service keycloak -n openftth | \
+                          grep LoadBalancer | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+')
+KEYCLOAK_REALM="master"
+KEYCLOAK_USER="user"
+KEYCLOAK_SECRET=$(kubectl get secret --namespace openftth keycloak-env-vars \
+                          -o jsonpath="{.data.KEYCLOAK_ADMIN_PASSWORD}" | base64 --decode)
+
+DIR_PATH=$(dirname $(realpath $0))
+REALM_FILE=$DIR_PATH"/realm.json"
+CLIENT_FILE=$DIR_PATH"/client.json"
+USER_FILE=$DIR_PATH"/user.json"
+CURL_CMD="curl --silent --show-error"
+
+#Receive token
+ACCESS_TOKEN=$(${CURL_CMD} \
+  -X POST \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "username=${KEYCLOAK_USER}" \
+  -d "password=${KEYCLOAK_SECRET}" \
+  -d "grant_type=password" \
+  -d 'client_id=admin-cli' \
+  "${KEYCLOAK_URL}/auth/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token" | jq -r '.access_token')
+
+# Create realm
+${CURL_CMD} \
+  -X POST \
+  -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d @"${REALM_FILE}" \
+  "${KEYCLOAK_URL}/auth/admin/realms";
+
+# Create client
+${CURL_CMD} \
+  -X POST \
+  -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d @"${CLIENT_FILE}" \
+  "${KEYCLOAK_URL}/auth/admin/realms/${NEW_REALM}/clients";
+
+# Create user
+${CURL_CMD} \
+  -X POST \
+  -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d @"${USER_FILE}" \
+  "${KEYCLOAK_URL}/auth/admin/realms/${NEW_REALM}/users";
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,7 @@ kubectl create namespace openftth
 # Install Strimzi
 helm repo add strimzi https://strimzi.io/charts/
 helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add bitnami https://charts.bitnami.com/bitnami
 
 helm repo update
 
@@ -14,6 +15,9 @@ helm install strimzi strimzi/strimzi-kafka-operator -n openftth --version 0.19
 
 # Install loki
 helm upgrade --install loki --namespace=openftth grafana/loki-stack --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false,loki.persistence.enabled=true,loki.persistence.storageClassName=standard,loki.persistence.size=10Gi --version 2.3.0
+
+# Install Keycloak
+helm upgrade --install keycloak bitnami/keycloak -n openftth --version 1.2.0
 
 # This is needed to make sure that the strimzi custom types are being registered
 sleep 1s
