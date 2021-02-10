9ad2a7e7ed43e4b6dce4021251f9067cdc80da35
Rune Nielsen <runenielsen@runbox.com>
2021-02-10 15:24:45 +0100

    TLS setup with lets-encrypt for nginx ingress (#32)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,17 +8,28 @@ helm repo add strimzi https://strimzi.io/charts/
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo add jetstack https://charts.jetstack.io
 
 helm repo update
 
 # Install strimzi
-helm install strimzi strimzi/strimzi-kafka-operator -n openftth --version 0.19
+helm install strimzi strimzi/strimzi-kafka-operator \
+     -n openftth \
+     --version 0.19
 
 # Install loki
 helm upgrade --install loki --namespace=openftth grafana/loki-stack --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false,loki.persistence.enabled=true,loki.persistence.storageClassName=standard,loki.persistence.size=10Gi --version 2.3.0
 
 # Install Keycloak
-helm upgrade --install keycloak bitnami/keycloak -n openftth --version 1.2.0 --set service.type=ClusterIP
+helm upgrade --install keycloak bitnami/keycloak -n openftth \
+     --version 1.2.0 \
+     --set service.type=ClusterIP
+
+# Install the cert-manager
+helm install cert-manager jetstack/cert-manager \
+  --namespace openftth \
+  --version v1.1.0 \
+  --set installCRDs=true
 
 # Install OpenFTTH
 helm install openftth openftth -n openftth
