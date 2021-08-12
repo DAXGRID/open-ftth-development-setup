e0f0147f4984e88d06f49ad483b05b66f0a4196f
Rune Nielsen <runenielsen@runbox.com>
2021-08-12 15:29:27 +0200

    upgrades danish address seed chart version

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -92,7 +92,7 @@ helm upgrade --install openftth-search dax/typesense \
 
 # Install Danish-address-seed
 helm upgrade --install danish-address-seed dax/danish-address-seed \
-     --version 1.1.6 \
+     --version 1.1.7 \
      --namespace openftth \
      --set schedule="0 0 * * *" \
      --set connectionString="Host=openftth-postgis;Port=5432;Username=postgres;Password=postgres;Database=OPEN_FTTH" \
