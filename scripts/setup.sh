76df96c92ce424eb1318901d2239d0b0a506aa6e
Rune Nielsen <runenielsen@runbox.com>
2021-10-27 16:09:03 +0200

    add file-changes services (#116)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,11 +8,15 @@ helm repo add strimzi https://strimzi.io/charts/
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo add jetstack https://charts.jetstack.io
 helm repo add dax https://daxgrid.github.io/charts
 
 # Update repos
 helm repo update
+
+# Install strimzi
+helm upgrade --install strimzi strimzi/strimzi-kafka-operator \
+     -n openftth \
+     --version 0.25.0
 
 # Install Nginx-Ingress
 helm install nginx-ingress ingress-nginx/ingress-nginx \
@@ -21,23 +25,11 @@ helm install nginx-ingress ingress-nginx/ingress-nginx \
     --create-namespace \
     --set controller.replicaCount=1
 
-# Install strimzi
-helm upgrade --install strimzi strimzi/strimzi-kafka-operator \
-     -n openftth \
-     --version 0.25.0
-
 # Install Keycloak
 helm upgrade --install keycloak bitnami/keycloak -n openftth \
      --version 2.3.0 \
      --set service.type=ClusterIP \
      --set proxyAddressForwarding=true
-
-# Install the cert-manager
-helm upgrade --install cert-manager jetstack/cert-manager \
-  --namespace cert-manager \
-  --create-namespace \
-  --version v1.3.1 \
-  --set installCRDs=true
 
 # Install Postgres database for OpenFTTH eventstore
 # Username and password should be changed in live env.
@@ -52,23 +44,56 @@ helm upgrade --install openftth-event-store bitnami/postgresql \
 # Install OpenFTTH
 helm install openftth openftth --namespace openftth
 
+# Install go-http-file-server
+helm upgrade --install file-server  dax/go-http-file-server \
+  --version 2.1.0 \
+  --namespace openftth \
+  --set username=user1 \
+  --set password=pass1
+
+cat <<EOF | kubectl apply -f -
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: file-server-ingress
+  namespace: openftth
+  annotations:
+    kubernetes.io/ingress.class: nginx
+    nginx.ingress.kubernetes.io/proxy-read-timeout: "3600"
+    nginx.ingress.kubernetes.io/proxy-send-timeout: "3600"
+    cert-manager.io/cluster-issuer: letsencrypt
+spec:
+  tls:
+  rules:
+  - host: files.openftth.local
+    http:
+      paths:
+      - path: /
+        backend:
+          serviceName: file-server-go-http-file-server
+          servicePort: 80
+EOF
+
 # Install Mbtileserver route-network
 helm upgrade --install openftth-routenetwork-tileserver dax/mbtileserver \
-  --version 3.2.0 \
+  --version 4.1.0 \
   --namespace openftth \
-  --set service.type=NodePort \
-  --set storage.size=1Gi \
+  --set watcher.enabled=true \
+  --set watcher.fileServer.username=user1 \
+  --set watcher.fileServer.password=pass1 \
+  --set watcher.fileServer.uri=http://file-server-go-http-file-server \
+  --set watcher.kafka.consumer=tile_watcher_route_network \
+  --set watcher.kafka.server=openftth-kafka-cluster-kafka-bootstrap:9092 \
+  --set "watcher.tileProcess.processes[0].name=TILEPROCESS__PROCESS__route_network.geojson" \
+  --set "watcher.tileProcess.processes[0].value=-z17 -pS -P -o /tmp/route_network.mbtiles /tmp/route_network.geojson --force --quiet" \
   --set 'commandArgs={--enable-reload-signal, --disable-preview, -d, /data}'
 
 # Install Mbtileserver base-map
 helm upgrade --install openftth-basemap-tileserver dax/mbtileserver \
-  --version 3.2.0 \
+  --version 4.1.0 \
   --namespace openftth \
   --set image.tag=danish-1621954230 \
-  --set service.type=NodePort \
-  --set storage.enabled=false \
-  --set 'commandArgs={--enable-reload-signal, -d, /tilesets}' \
-  --set reload.enabled=false
+  --set 'commandArgs={--enable-reload-signal, --disable-preview, -d, /tilesets}'
 
 # Install Typesense
 helm upgrade --install openftth-search dax/typesense \
@@ -104,15 +129,11 @@ helm upgrade --install relational-projector dax/relational-projector \
      --set geoDatabase.username=postgres \
      --set geoDatabase.username=postgres
 
-# Install Tippecanoe
-helm upgrade --install openftth-tilegenerator dax/tippecanoe \
-     --version 2.0.0 \
+# Route network tile data extract
+helm upgrade --install route-network-tile-data-extract dax/open-ftth-tile-data-extract \
+     --version 1.0.0 \
      --namespace openftth \
-     --set schedule="*/30 * * * *" \
-     --set commandArgs='sha1sum --ignore-missing -c /data/route_network.geojson.sha1 || (tippecanoe -z17 -pS -P -o ./route_network.mbtiles /data/route_network.geojson --force && cp ./route_network.mbtiles /data/route_network.mbtiles && sha1sum /data/route_network.geojson > /data/route_network.geojson.sha1)' \
-     --set storage.enabled=true \
-     --set storage.claimName=openftth-routenetwork-tileserver-mbtileserver \
-     --set prejob.enabled=true \
-     --set prejob.commandArgs='dotnet OpenFTTH.TileDataExtractor.dll "Host=openftth-postgis;Port=5432;Username=postgres;Password=postgres;Database=OPEN_FTTH" route_network.geojson && cp route_network.geojson /data/route_network.geojson' \
-     --set prejob.image.repository=openftth/tile-data-extract \
-     --set prejob.image.tag=v1.3.0
+     --set postgisConnectionString="Host=openftth-postgis;Port=5432;Username=postgres;Password=postgres;Database=OPEN_FTTH" \
+     --set fileServer.uri=http://file-server-go-http-file-server \
+     --set fileServer.username=user1 \
+     --set fileServer.password=pass1
