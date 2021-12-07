7eac98d8986f15c69ee2be3676c1d6f77740431b
Rune Nielsen <runenielsen@runbox.com>
2021-12-07 15:50:29 +0100

    upgrades relational projector to 1.0.9

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -192,7 +192,7 @@ helm upgrade --install route-network-search-indexer dax/route-network-search-ind
 
 # Install relational projector
 helm upgrade --install relational-projector dax/relational-projector \
-     --version 1.0.8 \
+     --version 1.0.9 \
      --namespace openftth \
      --set eventStoreDatabase.username=postgres \
      --set eventStoreDatabase.password=postgres \
