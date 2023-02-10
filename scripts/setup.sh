9395d8f0f66dde4959b69c4e3465ffd63858866e
runeanielsen <runenielsen@runbox.com>
2023-02-10 15:40:53 +0100

    remove using kafka domain route network

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -129,9 +129,9 @@ helm upgrade --install danish-address-seed dax/danish-address-seed \
 
 # Install Route-network-search-indexer
 helm upgrade --install route-network-search-indexer dax/route-network-search-indexer \
-     --version 1.4.0 \
+     --version 2.1.1 \
      --namespace openftth \
-     --set kafka.positionConnectionString="Host=openftth-postgis;Port=5432;Username=postgres;Password=postgres;Database=OPEN_FTTH" \
+     --set eventStore.connectionString="Host=openftth-postgis;Port=5432;Username=postgres;Password=postgres;Database=OPEN_FTTH" \
      --set typesense.apiKey=changeMe!
 
 # Install relational projector
