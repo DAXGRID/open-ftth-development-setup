6160b70950497145de6ee720985a940f15e9a796
Rune Nielsen <runenielsen@runbox.com>
2023-11-01 09:46:32 +0100

    upgrades keycloak chart version to 17.2.1 in setup script (#249)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -41,7 +41,7 @@ fi
 
 ## Disable readiness and liveness probe until we migrate away from /auth
 helm upgrade --install keycloak bitnami/keycloak -n openftth \
-     --version 17.0.0 \
+     --version 17.2.1 \
      --set service.type=ClusterIP \
      --set auth.adminPassword=$ADMIN_PASSWORD \
      --set production=true \
