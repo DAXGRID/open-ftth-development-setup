3570780ccda2e78a13c2c2f6bd94fb7c9a22a1f6
Rune Nielsen <runenielsen@runbox.com>
2023-12-27 10:28:08 +0100

    upgrade openftth frontend to version 1.1.17 in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -88,7 +88,7 @@ helm upgrade --install openftth-api-gateway dax/openftth-api-gateway \
 # Install OpenFTTH frontend
 helm upgrade --install openftth-frontend dax/openftth-frontend \
     -f scripts/openftth-frontend-override.yaml \
-    --version 1.1.14 \
+    --version 1.1.17 \
     --namespace openftth \
     --set-file maplibreJson=./settings/maplibre.json
 
