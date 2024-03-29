1e72d205207125f254804042c56e6240b1f8ce3d
runeanielsen <runenielsen@runbox.com>
2024-03-29 09:01:39 +0100

    upgrades openftth services in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -82,13 +82,13 @@ helm upgrade --install openftth openftth -n openftth
 # Install OpenFTTH api gateway
 helm upgrade --install openftth-api-gateway dax/openftth-api-gateway \
      -f scripts/openftth-api-gateway-override.yaml \
-     --version 1.1.12 \
+     --version 1.1.13 \
      --namespace openftth
 
 # Install OpenFTTH frontend
 helm upgrade --install openftth-frontend dax/openftth-frontend \
     -f scripts/openftth-frontend-override.yaml \
-    --version 1.1.20 \
+    --version 1.1.24 \
     --namespace openftth \
     --set-file maplibreJson=./settings/maplibre.json
 
