1cf8d1ad0419a361b6c198f8bc8a5672054932e5
runeanielsen <runenielsen@runbox.com>
2024-05-09 20:00:34 +0200

    upgrades keycloak to version 21.1.3

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -40,7 +40,7 @@ fi
 
 ## Disable readiness and liveness probe until we migrate away from /auth
 helm upgrade --install keycloak bitnami/keycloak -n openftth \
-     --version 19.3.4 \
+     --version 21.1.3 \
      --set service.type=ClusterIP \
      --set auth.adminPassword=$ADMIN_PASSWORD \
      --set production=true \
